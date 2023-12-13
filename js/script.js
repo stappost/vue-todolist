23d84863b4911287816e3606b04af46860377174
stappost <emanuele.scarpantoni@gmail.com>
2023-12-13 18:43:33 +0100

    add modify button/function

--- js/script.js
+++ js/script.js
@@ -9,22 +9,27 @@ createApp({
                 {
                     text:'fare la spesa',
                     done: false,
+                    modify: false,
                 },
                 {
                     text:'comprare il collare per il cane',
                     done: false,
+                    modify: false,
                 },
                 {
                     text:'portare a spasso il cane',
                     done: false,
+                    modify: false,
                 },
                 {
                     text:'rincorrere il cane',
                     done: false,
+                    modify: false,
                 },
                 {
                     text:'riportare il collare difettoso in negozio',
                     done: false,
+                    modify: false,
                 },
             ]
         }
@@ -33,6 +38,11 @@ createApp({
         add_task(){
             this.todoList.push({text:this.text_task, done:false})
             this.text_task= "";
+        },
+        modify_task(todo){
+            todo.text = this.text_task;
+            this.text_task= "";
+            todo.modify = false;
         }
     }
 }).mount('#app')
