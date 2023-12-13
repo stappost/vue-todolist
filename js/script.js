87669682d392bebbb942c76c9b4c9e3bc9c284a3
stappost <emanuele.scarpantoni@gmail.com>
2023-12-13 20:16:30 +0100

    add description

--- js/script.js
+++ js/script.js
@@ -4,38 +4,54 @@ createApp({
     data(){
         return{
             text_task: "",
+            descr_task: "",
             checked: 0,
             // TO DO LIST 
             todoList:[
                 {
                     text:'fare la spesa',
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, veritatis quia. Magnam odio debitis unde eius, quo numquam dolores cupiditate esse non, minima, repellendus fuga harum ipsam sint perspiciatis. Ipsam?",
                     done: false,
                     modify: false,
+                    modify_descr: false,
+                    read: false,
                     show: true,
                     
                 },
                 {
                     text:'comprare il collare per il cane',
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, veritatis quia. Magnam odio debitis unde eius, quo numquam dolores cupiditate esse non, minima, repellendus fuga harum ipsam sint perspiciatis. Ipsam?",
                     done: false,
                     modify: false,
+                    modify_descr: false,
+                    read: false,
                     show: true,
                 },
                 {
                     text:'portare a spasso il cane',
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, veritatis quia. Magnam odio debitis unde eius, quo numquam dolores cupiditate esse non, minima, repellendus fuga harum ipsam sint perspiciatis. Ipsam?",
                     done: false,
                     modify: false,
+                    modify_descr: false,
+                    read: false,
                     show: true,
                 },
                 {
                     text:'rincorrere il cane',
+                    description: "gLorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, veritatis quia. Magnam odio debitis unde eius, quo numquam dolores cupiditate esse non, minima, repellendus fuga harum ipsam sint perspiciatis. Ipsam?",
                     done: false,
                     modify: false,
+                    modify_descr: false,
+                    read: false,
                     show: true,
                 },
                 {
                     text:'riportare il collare difettoso in negozio',
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, veritatis quia. Magnam odio debitis unde eius, quo numquam dolores cupiditate esse non, minima, repellendus fuga harum ipsam sint perspiciatis. Ipsam?",
                     done: false,
                     modify: false,
+                    modify_descr: false,
+                    read: false,
                     show: true,
                 },
             ]
@@ -61,6 +77,11 @@ createApp({
                     todo.show = true
                 }
             });
-        }
+        },
+        modify_descr(todo){
+            todo.description = this.descr_task;
+            this.descr_task= "";
+            todo.modify_descr = false;
+        },
     }
 }).mount('#app')
