3318725372f6a47e9a08b809d89fa66433ff6182
stappost <emanuele.scarpantoni@gmail.com>
2023-12-13 13:41:26 +0100

    add array todoList

--- js/script.js
+++ js/script.js
@@ -3,7 +3,29 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-
+            // TO DO LIST 
+            todoList:[
+                {
+                    text:'fare la spesa',
+                    done: false,
+                },
+                {
+                    text:'comprare il collare per il cane',
+                    done: false,
+                },
+                {
+                    text:'portare a spasso il cane',
+                    done: false,
+                },
+                {
+                    text:'rincorrere il cane',
+                    done: false,
+                },
+                {
+                    text:'riportare il collare difettoso in negozio',
+                    done: false,
+                },
+            ]
         }
     },
     methods:{
