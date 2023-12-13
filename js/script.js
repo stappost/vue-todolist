2d755fa7138104b7c7d7c87cb24ba9fd1d30ab19
stappost <emanuele.scarpantoni@gmail.com>
2023-12-13 19:30:54 +0100

    add filtro checked

--- js/script.js
+++ js/script.js
@@ -4,32 +4,39 @@ createApp({
     data(){
         return{
             text_task: "",
+            checked: 0,
             // TO DO LIST 
             todoList:[
                 {
                     text:'fare la spesa',
                     done: false,
                     modify: false,
+                    show: true,
+                    
                 },
                 {
                     text:'comprare il collare per il cane',
                     done: false,
                     modify: false,
+                    show: true,
                 },
                 {
                     text:'portare a spasso il cane',
                     done: false,
                     modify: false,
+                    show: true,
                 },
                 {
                     text:'rincorrere il cane',
                     done: false,
                     modify: false,
+                    show: true,
                 },
                 {
                     text:'riportare il collare difettoso in negozio',
                     done: false,
                     modify: false,
+                    show: true,
                 },
             ]
         }
@@ -43,6 +50,17 @@ createApp({
             todo.text = this.text_task;
             this.text_task= "";
             todo.modify = false;
+        },
+        filtro(){
+            this.todoList.forEach(todo => {
+                if(!todo.done && this.checked == 1 || todo.done && this.checked == 2){
+                    todo.show = false
+                    console.log(todo.show)
+                }
+                else{
+                    todo.show = true
+                }
+            });
         }
     }
 }).mount('#app')
